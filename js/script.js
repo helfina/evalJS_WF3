2b7f26f9277473c396a2411eb286065f68dd476d
gaelle kerforne <stage.seeweb@gmail.com>
2022-02-02 13:52:42 +0100

    menu buger js et debut datapicker html
    Took 1 hour 4 minutes

--- js/script.js
+++ js/script.js
@@ -2,8 +2,22 @@
 let menuBurger = document.getElementById('menu-burger');
 console.log(menuBurger);
 
-menuBurger.addEventListener('click', function (){
+menuBurger.addEventListener('click', function () {
     let menu = document.getElementById('menu');
+    let icon = document.querySelector('i');
+    console.log(icon);
     const style = getComputedStyle(menu);
     console.log('1 entrer event: ' + style.display);
-})
+    if (style.display == 'none') {
+        menu.style.display = 'block';
+        menu.style.top = '10px';
+        menuBurger.style.backgroundColor = 'white';
+        icon.style.color = 'black';
+        console.log('2 entrer if: ' + style.display);
+    } else {
+        menu.style.display = 'none';
+        menuBurger.style.removeProperty("background-color");
+        icon.style.color = 'white';
+        console.log('3 entrer else : ' + style.display);
+    }
+});
